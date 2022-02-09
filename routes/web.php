e426c0ea28dfb2362f3ac3edfa466c511118a155
cangussu12 <leo.tywin@gmail.com>
2022-02-09 17:38:06 -0300

    finalizando rotas

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,29 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos',[\App\Http\Controllers\SobrenosController::class, 'sobrenos'])->name('site.sobrenos');
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login';})->name('site.login');
 
-Route::get('/sobrenos',[\App\Http\Controllers\SobrenosController::class, 'sobrenos']);
 
-Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes';});
+    Route::get('/fornecedores', function() { return 'Fornecedores';});
+    Route::get('/produtos', function() { return 'Produtos';});
+});
 
+Route::get('/rota1', function() {
+    echo 'rota 1';
+})->name('site.rota1');
+    
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+//Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a pagina inicial.';
+});
