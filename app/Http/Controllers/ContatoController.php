574c8416b28b3eda9d9fb31a885c0bc45b07a7e5
cangussu12 <leo.tywin@gmail.com>
2022-03-12 22:46:18 -0300

    personalização formularios

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -18,14 +18,36 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
        
-        $request->validate([
-            'nome' => 'required|min:3|max:40', //nomes com no minimo 3 caracteres e no maximo 40
-            'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+        $regras = [
+            'nome' => 'required|min:3|max:40|', //nomes com no minimo 3 caracteres e no maximo 40
+            'telefone' => 'required|unique:site_contatos',
+            'email' => 'email|unique:site_contatos',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
-        // SiteContato::create($request->all());
+        ];
+
+        $feedback = [
+
+            'nome.required' => 'O campo nome precisa ser preenchido',
+            'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
+            'nome.max' => 'O campo nome só é permitido 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+            'telefone.unique' => 'O telefone informado já esta em uso',
+
+            'required' => 'O campo :attribute deve ser preenchido',
+
+            'email.unique' => 'O e-mail informado já está em uso',
+            'email.email' => 'O email informado não é valido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2.000 caracteres'
+
+        ];
+
+
+        $request->validate($regras, $feedback);
+    
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
 
     }
 }
